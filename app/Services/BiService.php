4395d39e1a81432318cd378b1fee83a51d158cba
yuxuchen <yuxuchen@thinktown.com>
2021-03-02 12:12:47 +0800

    初始化Bi服务类

--- app/Services/BiService.php
+++ app/Services/BiService.php
@@ -4,7 +4,18 @@
 namespace App\Services;
 
 
+use GuzzleHttp\Client;
+
 class BiService
 {
 
+    public function __construct()
+    {
+
+    }
+
+    public function connectiong2hupun()
+    {
+        $client = new Client();
+    }
 }
