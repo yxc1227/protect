4ff3c0fbf1c96cfa0be64c44ccbcc1bc6299857a
yuxuchen <yuxuchen@thinktown.com>
2021-03-06 20:21:17 +0800

    加入报错日志方便查错

--- app/Services/HupunService.php
+++ app/Services/HupunService.php
@@ -25,7 +25,7 @@ class HupunService
      */
     public function connectiong2hupun($uri = '', $customParams = [], $time = ''): string
     {
-        if(!$time){
+        if (!$time) {
             $time = time();
         }
         $client = new Client([
@@ -41,10 +41,10 @@ class HupunService
         $data['_sign'] = $this->createSign($data);
         try {
             $response = $client->request('POST', $uri, ['form_params' => $data]);
-            return (String) $response->getBody();
+            return (string)$response->getBody();
         } catch (Throwable $e) {
             $message = $e->getMessage();
-            Log::channel('hupun')->error($message,['uri'=>$uri,'data'=>$data]);
+            Log::channel('hupun')->error($message, ['uri' => $uri, 'data' => $data]);
             return '';
         }
     }
@@ -79,21 +79,21 @@ class HupunService
      * 获取并保存原始请求结果
      * @return int
      */
-    public function getHupunTrades():int
+    public function getHupunTrades(): int
     {
         $now = time();
-        $res = HupunTradesRawModel::select('timestamp')->orderby('timestamp','desc')->first();
-        if($res){
+        $res = HupunTradesRawModel::select('timestamp')->orderby('timestamp', 'desc')->first();
+        if ($res) {
             $time = $res->timestamp;
         } else {
             //万里牛约定仅支持最近三个月的数据，但是实际发现有漏洞可以多拉几个月
-            $time= strtotime(date("Y-m-d H:i:s", strtotime("-24 month")));
+            $time = strtotime(date("Y-m-d H:i:s", strtotime("-24 month")));
         }
         $uri = '/api/erp/opentrade/list/trades';
         //业务请求参数，大部分都没啥用
         $customParams = [
 //            'bill_code'=>'P2020083100002367957',
-            'create_time'=> $time * 1000,
+            'create_time' => $time * 1000,
 //            'modify_time'=>'',
 //            'send_goods_time'=>'',
 //            'finish_time'=>'',
@@ -108,20 +108,20 @@ class HupunService
 //            'trade_status' => '',
         ];
         $flag = true;
-        while ($flag){
-            $result = $this->connectiong2hupun($uri,$customParams,$now);
-            if(!$result){
+        while ($flag) {
+            $result = $this->connectiong2hupun($uri, $customParams, $now);
+            if (!$result) {
                 break;
                 return 0;
             }
-            $content = json_decode($result,true);
-            if($content['code'] != 0 || empty($content['data'])){
+            $content = json_decode($result, true);
+            if ($content['code'] != 0 || empty($content['data'])) {
                 $flag = false;
             }
-            if(!empty($content['data'])){
-                HupunTradesRawModel::create(['timestamp'=>$now,'raw'=>json_encode($content['data'])]);
+            if (!empty($content['data'])) {
+                HupunTradesRawModel::create(['timestamp' => $now, 'raw' => json_encode($content['data'])]);
             }
-            $customParams['page'] ++;
+            $customParams['page']++;
         }
         return $now;
 
@@ -131,59 +131,64 @@ class HupunService
     /**
      *解析接口请求到的原始数据
      */
-    public function resolveHupunTradesRaw():void
+    public function resolveHupunTradesRaw(): void
     {
         $raws = HupunTradesRawModel::where('resolved','=',0)->get();
-        if(!$raws){
+        if (!$raws) {
             return;
         }
         $raws->toArray();
-        foreach ($raws as $raw){
-            $rawId = Arr::get($raw,'id');
-            $info = Arr::get($raw,'raw');
-            $trades = json_decode($info,true);
-            foreach ($trades as $trade){
-                $trade['rawid'] = $rawId;
+        try {
+            foreach ($raws as $raw) {
+                $rawId = Arr::get($raw, 'id');
+                $info = Arr::get($raw, 'raw');
+                $trades = json_decode($info, true);
+                foreach ($trades as $trade) {
+                    $trade['rawid'] = $rawId;
 
-                //json扩展字段做特殊处理防止格式错误
-                $oln_order_list = Arr::get($trade,'oln_order_list');
-                if($oln_order_list){$trade['oln_order_list'] = json_encode($oln_order_list);}
-                $trade_extend = Arr::get($trade,'trade_extend');
-                if($trade_extend){$trade['trade_extend'] = json_encode($trade_extend);}
+                    //json扩展字段做特殊处理防止格式错误
+                    $oln_order_list = Arr::get($trade, 'oln_order_list');
+                    $trade['oln_order_list'] = json_encode($oln_order_list);
+                    $trade_extend = Arr::get($trade, 'trade_extend');
+                    $trade['trade_extend'] = json_encode($trade_extend);
 
-                //两个通过关系关联的对象要提前摘出
-                $invoice = Arr::get($trade,'invoice');
-                if($invoice){unset($trade['invoice']);}
-                $orders = Arr::get($trade,'orders');
-                if($orders){unset($trade['orders']);}
+                    //两个通过关系关联的对象要提前摘出
+                    $invoice = Arr::get($trade, 'invoice');
+                    unset($trade['invoice']);
+                    $orders = Arr::get($trade, 'orders');
+                    unset($trade['orders']);
 
-                //13位时间戳需要特殊处理
-                Arr::get($trade,'create_time') ? $trade['create_time'] = date("Y-m-d H:i:s",$trade['create_time'] / 1000) : null;
-                Arr::get($trade,'modify_time') ? $trade['modify_time'] = date("Y-m-d H:i:s",$trade['modify_time'] / 1000) : null;
-                Arr::get($trade,'pay_time') ? $trade['pay_time'] = date("Y-m-d H:i:s",$trade['pay_time'] / 1000) : null;
-                Arr::get($trade,'end_time') ? $trade['end_time'] = date("Y-m-d H:i:s",$trade['end_time'] / 1000) : null;
-                Arr::get($trade,'print_time') ? $trade['print_time'] = date("Y-m-d H:i:s",$trade['print_time'] / 1000) : null;
-                Arr::get($trade,'send_time') ? $trade['send_time'] = date("Y-m-d H:i:s",$trade['send_time'] / 1000) : null;
+                    //13位时间戳需要特殊处理
+                    Arr::get($trade, 'create_time') ? $trade['create_time'] = date("Y-m-d H:i:s", $trade['create_time'] / 1000) : null;
+                    Arr::get($trade, 'modify_time') ? $trade['modify_time'] = date("Y-m-d H:i:s", $trade['modify_time'] / 1000) : null;
+                    Arr::get($trade, 'pay_time') ? $trade['pay_time'] = date("Y-m-d H:i:s", $trade['pay_time'] / 1000) : null;
+                    Arr::get($trade, 'end_time') ? $trade['end_time'] = date("Y-m-d H:i:s", $trade['end_time'] / 1000) : null;
+                    Arr::get($trade, 'print_time') ? $trade['print_time'] = date("Y-m-d H:i:s", $trade['print_time'] / 1000) : null;
+                    Arr::get($trade, 'send_time') ? $trade['send_time'] = date("Y-m-d H:i:s", $trade['send_time'] / 1000) : null;
 
-                $trade_record = new HupunTradesModel();
-                $trade_record->timestamps = false;
-                $trade_record->forceFill($trade)->save();
-                $tradeId = $trade_record->id;
-                foreach ($orders as $order){
-                    $order['trades_id'] = $tradeId;
-                    $order_record = new HupunTradesOrdersModel();
-                    $order_record->timestamps = false;
-                    $order_record->forceFill($order)->save();
+                    $trade_record = new HupunTradesModel();
+                    $trade_record->timestamps = false;
+                    $trade_record->forceFill($trade)->save();
+                    $tradeId = $trade_record->id;
+                    foreach ($orders as $order) {
+                        $order['trades_id'] = $tradeId;
+                        $order_record = new HupunTradesOrdersModel();
+                        $order_record->timestamps = false;
+                        $order_record->forceFill($order)->save();
+                    }
+                    if ($invoice) {
+                        $invoice['trades_id'] = $tradeId;
+                        $invoice_record = new HupunTradesInvoiceModel();
+                        $invoice_record->timestamps = false;
+                        $invoice_record->forceFill($invoice)->save();
+                    }
                 }
-                if($invoice){
-                    $invoice['trades_id'] = $tradeId;
-                    $invoice_record = new HupunTradesInvoiceModel();
-                    $invoice_record->timestamps = false;
-                    $invoice_record->forceFill($invoice)->save();
-                }
+                $raw->resolved = 1;
+                $raw->save();
             }
-            $raw->resolved = 1;
-            $raw->save();
+        } catch (Throwable $e) {
+            $message = $e->getMessage();
+            Log::channel('hupun')->error($message, ['trade' => $trade, 'order' => $order]);
         }
     }
 }
