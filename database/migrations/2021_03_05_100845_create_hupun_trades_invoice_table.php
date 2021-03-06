8af8098a904292fcd4a44733d5358ce0ac1d284d
yuxuchen <yuxuchen@thinktown.com>
2021-03-06 10:13:45 +0800

    统一标点符号

--- database/migrations/2021_03_05_100845_create_hupun_trades_invoice_table.php
+++ database/migrations/2021_03_05_100845_create_hupun_trades_invoice_table.php
@@ -15,11 +15,11 @@ class CreateHupunTradesInvoiceTable extends Migration
     {
         Schema::create('hupun_trades_invoice', function (Blueprint $table) {
             $table->id();
-            $table->unsignedInteger('trades_id')->comment("对应tradesid");
+            $table->unsignedInteger('trades_id')->comment('对应tradesid');
             $table->string('header')->default('')->comment('发票抬头');
             $table->string('invoice_detail')->default('')->comment('发票详情');
             $table->string('invoice_tax_id')->default('')->comment('税号');
-            $table->integer("type")->default(0)->comment("类型:1=普通发票，2=增值税普通发票, 3=电子增票");
+            $table->integer('type')->default(0)->comment('类型:1=普通发票，2=增值税普通发票, 3=电子增票');
         });
     }
 
