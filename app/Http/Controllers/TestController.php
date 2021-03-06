612f403fc8a226e80882cebbdf0d0ac1fe54b353
yuxuchen <yuxuchen@thinktown.com>
2021-03-06 21:30:38 +0800

    关闭测试控制器中的内容

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,22 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use App\Services\BiService;
+
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    private $biService;
 
-    public function __construct(BiService $biService)
+    public function __construct()
     {
-        $this->biService = $biService;
     }
 
     public function index(Request $request)
     {
-        $this->biService->connectiong2hupun();
-
         return $this->response(100);
     }
 }
